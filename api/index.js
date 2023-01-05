709bb1b4a90789917e78293712e818298bf4809c
Christian John Guipo <cjohnguipo@gmail.com>
2023-01-05 15:50:36 +0800

    update

--- api/index.js
+++ api/index.js
@@ -3,11 +3,17 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+})
 
 app.use("/auth", require("./routes/auth"));
 app.use("/posts", require("./routes/posts"));
 
+
 app.listen(5000, () => {
   console.log("Listening on port 5000");
 });
+
+module.exports = app
